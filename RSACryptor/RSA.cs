41bb1e1fb0a39ca86a54011f89637a286a84aabb
kreuwont <icdg@yandex.ru>
2018-09-09 14:40:10 +0300

    Upload files

---
+++ RSACryptor/RSA.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Cryptography;
+
+namespace RSACryptor
+{
+    using Buff = System.Byte;
+    using Length = System.Int32;
+    [SecuritySafeCritical]
+    public class RSACrypt : IDisposable
+    {
+        private const Length RecommendKeyLength = 2048;
+
+        private int _keyLength;
+        private byte[] _publicKey;
+        private byte[] _privateKey;
+
+        [SecuritySafeCritical]
+        public RSACrypt() : this(RecommendKeyLength)
+        {
+        }
+
+        [SecuritySafeCritical]
+        public RSACrypt(Length keyLength)
+        {
+            _keyLength = keyLength;
+            GenerateKey();
+        }
+
+        /// <summary>
+        /// Генерирует новые приватный и публичный ключи
+        /// </summary>
+        [SecuritySafeCritical]
+        public void GenerateKey()
+        {
+            ClearKey();
+            using (var provider = new RSACryptoServiceProvider(_keyLength))
+            {
+                _publicKey = provider.ExportCspBlob(false);
+                _privateKey = provider.ExportCspBlob(true);
+            }
+        }
+
+        [SecuritySafeCritical]
+        public Buff[] Encrypt(Buff[] buffer)
+        {
+            List<byte> retnBuff = new List<byte>();
+
+            using (var rsaProvider = new RSACryptoServiceProvider(_keyLength))
+            {
+                rsaProvider.ImportCspBlob(_publicKey);
+
+                int maxLength = (_keyLength - 384) / 8 + 37;
+                int dataLength = buffer.Length;
+                int iterations = dataLength == maxLength ? 0 : dataLength / maxLength;
+                for (int i = 0; i <= iterations; i++)
+                {
+                    byte[] tempBytes = new byte[
+                            (dataLength - maxLength * i > maxLength) ? maxLength :
+                                                          dataLength - maxLength * i];
+                    Buffer.BlockCopy(buffer, maxLength * i, tempBytes, 0,
+                                      tempBytes.Length);
+
+                    byte[] encryptBuffer = rsaProvider.Encrypt(tempBytes, false);
+
+                    retnBuff.AddRange(encryptBuffer);
+                }
+            }
+
+            return retnBuff.ToArray();
+        }
+
+        [SecuritySafeCritical]
+        public Buff[] Decrypt(Buff[] buffer)
+        {
+            List<byte> retnBuff = new List<byte>();
+            using (var rsaProvider = new RSACryptoServiceProvider(_keyLength))
+            {
+                try
+                {
+                    rsaProvider.ImportCspBlob(_privateKey);
+
+                    int maxLength = _keyLength / 8;
+                    int dataLength = buffer.Length;
+                    int iterations = dataLength == maxLength ? 1 : dataLength / maxLength;
+                    for (int i = 0; i < iterations; i++)
+                    {
+                        byte[] tempBytes = new byte[
+                                (dataLength - maxLength * i > maxLength) ? maxLength :
+                                                              dataLength - maxLength * i];
+                        Buffer.BlockCopy(buffer, maxLength * i, tempBytes, 0,
+                                          tempBytes.Length);
+
+                        byte[] decryptedBuffer = rsaProvider.Decrypt(tempBytes, false);
+
+                        retnBuff.AddRange(decryptedBuffer);
+                    }
+                }
+                catch (CryptographicException)
+                {
+                    throw;
+                }
+            }
+
+            return retnBuff.ToArray();
+        }
+
+        [SecuritySafeCritical]
+        public Buff[] ExportKey(bool isPrivate)
+        {
+            if (isPrivate)
+                return _privateKey;
+            else
+                return _publicKey;
+        }
+
+        [SecuritySafeCritical]
+        public void ImportKey(Length keyLength, Buff[] key, bool isPrivate)
+        {
+            if (keyLength == 0 || keyLength % 2 != 0)
+                throw new ArgumentException("Некорректный размер ключа");
+
+            if (key == null)
+                throw new ArgumentNullException($"{nameof(key)} не может быть равно null");
+
+            _keyLength = keyLength;
+
+            ClearKey();
+
+            if (isPrivate)
+            {
+                _privateKey = new byte[key.Length];
+                Array.Copy(key, 0, _privateKey, 0, key.Length);
+            }
+            else
+            {
+                _publicKey = new byte[key.Length];
+                Array.Copy(key, 0, _publicKey, 0, key.Length);
+            }
+        }
+
+        [SecuritySafeCritical]
+        public Length ExportKeyLenght()
+        {
+            return _keyLength;
+        }
+
+        [SecuritySafeCritical]
+        private void ClearKey()
+        {
+            if (_privateKey != null)
+            {
+                Array.Clear(_privateKey, 0, _privateKey.Length);
+                _privateKey = null;
+            }
+
+            if (_publicKey != null)
+            {
+                Array.Clear(_publicKey, 0, _publicKey.Length);
+                _publicKey = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            ClearKey();
+        }
+    }
+}
---
+++ RSACryptor/RSAFileCryptor.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSACryptor
+{
+    public enum Opcode : byte
+    {
+        KeyLength = 0,
+        PublicKey = 1,
+        PrivateKey = 2
+    }
+
+    [Flags]
+    public enum DataTypeFlag
+    {
+        PublicKey = 1,
+        PrivateKey = 2
+    }
+
+    public static class RSAFileCryptor
+    {
+        public static async Task EncryptFileAsync(string filePath, string saveKeyDataPath, DataTypeFlag saveInfoFlag)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("File not found");
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.Read))
+            {
+                using (var provider = new RSACrypt())
+                {
+                    byte[] fileBuffer = new byte[stream.Length];
+                    await stream.ReadAsync(fileBuffer, 0, fileBuffer.Length);
+
+                    var encryptBuffer = provider.Encrypt(fileBuffer);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    stream.SetLength(encryptBuffer.Length);
+                    await stream.WriteAsync(encryptBuffer, 0, encryptBuffer.Length);
+
+                    Array.Clear(fileBuffer, 0, fileBuffer.Length);
+                    SaveKeyData(provider, saveKeyDataPath, saveInfoFlag);
+                }
+            }
+        }
+
+        public static async Task DecryptFileAsync(string filePath, string keyDataPath)
+        {
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+            {
+                using (var provider = new RSACrypt())
+                {
+                    (int keyLenght, byte[] publicKey, byte[] privateKey) = ReadKeyData(provider, keyDataPath, DataTypeFlag.PrivateKey);
+                    provider.ImportKey(keyLenght, privateKey, true);
+
+                    byte[] fileBuffer = new byte[stream.Length];
+                    await stream.ReadAsync(fileBuffer, 0, fileBuffer.Length);
+
+                    var decryptBuffer = provider.Decrypt(fileBuffer);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    stream.SetLength(decryptBuffer.Length);
+                    await stream.WriteAsync(decryptBuffer, 0, decryptBuffer.Length);
+
+                    Array.Clear(fileBuffer, 0, fileBuffer.Length);
+                }
+            }
+        }
+
+        private static void SaveKeyData(RSACrypt provider, string path, DataTypeFlag saveInfoFlag)
+        {
+            using (var bw = new BinaryWriter(File.Create(path)))
+            {
+                bw.Write((byte)Opcode.KeyLength);
+                bw.Write(provider.ExportKeyLenght());
+
+                if (Convert.ToBoolean(saveInfoFlag & DataTypeFlag.PublicKey))
+                {
+                    var key = provider.ExportKey(false);
+                    bw.Write((byte)Opcode.PublicKey);
+                    bw.Write(key.Length);
+                    bw.Write(key);
+                }
+
+                if (Convert.ToBoolean(saveInfoFlag & DataTypeFlag.PrivateKey))
+                {
+                    var key = provider.ExportKey(true);
+                    bw.Write((byte)Opcode.PrivateKey);
+                    bw.Write(key.Length);
+                    bw.Write(key);
+                }
+            }
+        }
+
+        private static (int, byte[], byte[]) ReadKeyData(RSACrypt provider, string path, DataTypeFlag saveTypeFlag)
+        {
+            int? keyLenght = null;
+            byte[] publicKey = null;
+            byte[] privateKey = null;
+
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Key file not found");
+
+            using (var br = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                while (br.BaseStream.Position < br.BaseStream.Length)
+                {
+                    Opcode opcode = (Opcode)br.ReadByte();
+                    switch (opcode)
+                    {
+                        case Opcode.KeyLength:
+                            if (keyLenght == null) keyLenght = br.ReadInt32();
+                            break;
+
+                        case Opcode.PublicKey:
+                            if (publicKey == null
+                                && Convert.ToBoolean(saveTypeFlag & DataTypeFlag.PublicKey))
+                                publicKey = br.ReadBytes(br.ReadInt32());
+                            break;
+
+                        case Opcode.PrivateKey:
+                            if (privateKey == null
+                                && Convert.ToBoolean(saveTypeFlag & DataTypeFlag.PrivateKey))
+                                privateKey = br.ReadBytes(br.ReadInt32());
+                            break;
+
+                        default:
+                            throw new Exception("Detected unknown opcode");
+                    }
+                }
+            }
+
+            if (keyLenght == null)
+                throw new Exception("Key lenght fault");
+
+            return (keyLenght.Value, publicKey, privateKey);
+        }
+    }
+}
