f3cab2801d4623cec62836fefdb8c30089efeab0
Ilya <icdg@yandex.ru>
2019-07-03 03:01:24 +0300

    Refactor tests

--- RSACryptor.Tests/RSACrypt.Tests.cs
+++ RSACryptor.Tests/RSACrypt.Tests.cs
@@ -14,33 +14,31 @@ namespace RSACryptor.Tests
         [DataRow("Random message for test RSA crypto system")]
         public void RSATests_String_EncryptDecrypt(string text)
         {
-            RSACrypt cryptor = new RSACrypt();
+            using (var cryptor = new RSACrypt())
+            {
+                var cryptBuffer = Encoding.UTF8.GetBytes(text);
+                var encryptBuffer = cryptor.Encrypt(cryptBuffer);
+                var decryptBuffer = cryptor.Decrypt(encryptBuffer);
+                var resultString = Encoding.UTF8.GetString(decryptBuffer);
 
-            var cryptBuffer = Encoding.UTF8.GetBytes(text);
-            var encryptBuffer = cryptor.Encrypt(cryptBuffer);
-            var decryptBuffer = cryptor.Decrypt(encryptBuffer);
-            var resultString = Encoding.UTF8.GetString(decryptBuffer);
-
-            cryptor.Dispose();
-
-            Assert.AreEqual(resultString, text);
+                Assert.AreEqual(resultString, text);
+            }
         }
 
         [DataTestMethod]
         [DataRow("512kb.txt")]
         public void RSATests_BigString_EncryptDecrypt(string fileName)
         {
-            string text = File.ReadAllText(fileName);
-            RSACrypt cryptor = new RSACrypt();
+            using (var cryptor = new RSACrypt())
+            {
+                var text = File.ReadAllText(fileName);
+                var cryptBuffer = Encoding.UTF8.GetBytes(text);
+                var encryptBuffer = cryptor.Encrypt(cryptBuffer);
+                var decryptBuffer = cryptor.Decrypt(encryptBuffer);
+                var resultString = Encoding.UTF8.GetString(decryptBuffer);
 
-            var cryptBuffer = Encoding.UTF8.GetBytes(text);
-            var encryptBuffer = cryptor.Encrypt(cryptBuffer);
-            var decryptBuffer = cryptor.Decrypt(encryptBuffer);
-            var resultString = Encoding.UTF8.GetString(decryptBuffer);
-
-            cryptor.Dispose();
-
-            Assert.AreEqual(resultString, text);
+                Assert.AreEqual(resultString, text);
+            }
         }
     }
 }
